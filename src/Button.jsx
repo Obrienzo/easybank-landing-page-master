e353ea5f332a83d09a27ea38b687717bc5e6586a
Disney <disneyconstant996@gmail.com>
2024-06-30 22:32:54 +0300

    Add html symbol on button

--- src/Button.jsx
+++ src/Button.jsx
@@ -2,7 +2,7 @@
 
 function Button() {
     return (
-        <button className="bg-gradient-to-r from-LimeGreen to-BrightCyan px-9 py-3 text-White font-normal rounded-3xl">Request Invite</button>
+        <button className="bg-gradient-to-r from-LimeGreen to-BrightCyan px-9 py-3 text-White font-normal rounded-3xl">Request Invite &rarr;</button>
     );
 }
 
