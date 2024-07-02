406a6a72e34fcbe5745569ff6bbde99c0a5a8182
Disney <disneyconstant996@gmail.com>
2024-07-02 09:35:21 +0300

    Add new layout

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -4,12 +4,12 @@ function HeroSection() {
 
     return (
         <section className="mb-20 bg-White relative">
-            <div className="md:flex md:flex-row-reverse md:mb-0 max-w-7xl mx-auto">
+            <div className="lg:flex lg:flex-row-reverse lg:mb-0 max-w-7xl mx-auto">
                 <picture>
-                    <source srcSet="/src/assets/bg-intro-mobile.svg" media="(max-width: 768px)"/>
-                    <img src="/src/assets/bg-intro-desktop.svg" alt="hero background" className="relative -right-40 -bottom-32" />
+                    <source srcSet="/src/assets/bg-intro-mobile.svg" media="(max-width: 976px)"/>
+                    <img src="/src/assets/bg-intro-desktop.svg" alt="hero background" className="lg:relative lg:-right-40 lg:-bottom-32 w-full" />
                 </picture>
-                <img src="/src/assets/image-mockups.png" alt="website mockups" className="absolute -top-24 md:-right-32 md:-top-36 md:absolute" />
+                <img src="/src/assets/image-mockups.png" alt="website mockups" className="absolute -top-24 right-0 left-0 mx-auto lg:-right-32 lg:-top-36 lg:absolute" />
                 <article className="text-center px-4 md:col-start-1 md:row-start-1 md:self-center md:text-start md:flex-grow-0">
                     <h1 className="font-normal text-4xl mb-4 lg:text-6xl">Next generation digital banking</h1>
                     <p className="text-Grayishblue mb-7">
